2b17793802e7fc2cf03633a11da80bf87112785b
Gavin Seegoolam <22893238+gavinlive@users.noreply.github.com>
2020-10-19 16:41:39 +0100

    init

---
+++ tasksetter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+n_gpus=$1
+
+if [ "$n_gpus" = "" ];
+then
+  n_gpus=9
+fi
+
+for this_gpu in $(seq 0 $n_gpus);
+do
+  pid=$(nvidia-smi -g $this_gpu | awk '$2=="Processes:" {p=1} p && $3 > 0 {print $3}')
+  if [ "$pid" = "" ] || [[ "$pid" == *"running"* ]];
+  then
+    echo "No PID"
+  else
+    while IFS=' ' read -ra ADDR; do
+          for i in "${ADDR[@]}"; do
+              # process "$i"
+              do_nothing_here=""
+          done
+    done <<< "$pid"
+    pid=$i
+    gpu_start=$((4*this_gpu))
+    gpus=$gpu_start,$(($gpu_start+1)),$(($gpu_start+2)),$(($gpu_start+3))
+    taskset -p -c $gpus $pid
+  fi
+done
